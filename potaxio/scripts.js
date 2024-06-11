2b5415f8ddce798c4a574f85d1e2c55df1269f8e
AniMuoz <165214399+AniMuoz@users.noreply.github.com>
2024-06-11 15:28:33 -0400

    Add files via upload

---
+++ potaxio/scripts.js
@@ -0,0 +1,19 @@
+
+document.getElementById('add-to-cart-btn').addEventListener('click', function() {
+    document.getElementById('payment-modal').style.display = 'block';
+});
+
+document.getElementById('pay-btn').addEventListener('click', function() {
+    // Redirige a la página de WebPay
+    window.location.href = 'https://webpay.com'; // Reemplaza con la URL correcta de WebPay
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('payment-modal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('payment-modal')) {
+        document.getElementById('payment-modal').style.display = 'none';
+    }
+});
