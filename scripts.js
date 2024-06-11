d1dc62940738861fb226298747bd40c7532cb80c
AniMuoz <165214399+AniMuoz@users.noreply.github.com>
2024-06-11 13:46:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ document.getElementById('add-to-cart-btn').addEventListener('click', function() 
     document.getElementById('payment-modal').style.display = 'block';
 });
 
+document.getElementById('pay-btn').addEventListener('click', function() {
+    // Redirige a la página de WebPay
+    window.location.href = 'https://webpay.com'; // Reemplaza con la URL correcta de WebPay
+});
+
 document.querySelector('.close').addEventListener('click', function() {
     document.getElementById('payment-modal').style.display = 'none';
 });
